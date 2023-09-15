466e0b1045081eee85095e2cc6e7a4152f53a4b7
brksaian <braianviana@hotmail.com>
2023-09-15 00:18:53 -0300

    Finalização do back

--- upload-ai-api/src/routes/generate-ai-completion.ts
+++ upload-ai-api/src/routes/generate-ai-completion.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 import { createReadStream } from "fs";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
-  app.post("/ai/complete", async (req) => {
+  app.post("/ai/complete", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
       template: z.string(),
@@ -13,5 +13,29 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
     });
 
     const { videoId, template, temperature } = bodySchema.parse(req.body);
+
+    const video = await prisma.video.findUnique({
+      where: {
+        id: videoId,
+      },
+    });
+
+    if (!video?.transcription) {
+      return reply.status(400).send({ error: "Transcription not found" });
+    }
+
+    const promptMessage = template.replace(
+      "{transcription}",
+      video.transcription
+    );
+
+    const response = await openia.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: promptMessage }],
+      temperature,
+    });
+    return {
+      response,
+    };
   });
 }
